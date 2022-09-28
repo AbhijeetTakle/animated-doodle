be505cab8a7d16addc964f5c504d19fec6cd12f3
Abhijeet Takle <abhijeettakle35@gmail.com>
2022-09-28 11:26:46 +0530

    added env file

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const { router } = require("./routes/router");
 const mongoose = require("mongoose");
 const { userModel } = require("./models/usersmodel");
+const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv");
 
-const url =
-  "mongodb+srv://abhijeettakle:zDmFnrARG52g2qQJ@ecomcluster.bwhyju4.mongodb.net/EcommerceDatabase?retryWrites=true&w=majority";
+app.use(cors());
+dotenv.config();
+
+const username = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+
+const url = `mongodb+srv://${username}:${password}@ecomcluster.bwhyju4.mongodb.net/EcommerceDatabase?retryWrites=true&w=majority`;
 const connectionParams = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
