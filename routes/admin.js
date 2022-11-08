0b70d3ff1edc731bc8f27bc557424d04f405cf33
Abhijeet Takle <abhijeettakle35@gmail.com>
2022-11-08 10:46:17 +0530

    admin updated

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
-const { createUser } = require("../controllers/user");
 const { getAdmin, deleteAdmin } = require("../controllers/admin");
 
-router.route("/api/v1/user").post(createUser).delete(deleteAdmin);
+router.route("/api/v1/admin").delete(deleteAdmin);
 router.route("/api/v1/login/admin/:email/:password").get(getAdmin);
 
 module.exports = router;
